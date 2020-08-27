cf0dfa444c884c00666590b998e45759183a58ff
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2020-08-27 11:03:15 +0200

    .

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,26 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
+            var age = 0;
+            Console.WriteLine("Hallå där! Hur gammal är du?");
+            var input = Console.ReadLine();
+            age = int.Parse(input);
+            Console.WriteLine("Vad heter du?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Lever du? (No / Yes)");
+            bool alive = false;
+            if (Console.ReadLine() == "Yes") {
+                alive = true;
+            }
+
+            Console.WriteLine("Du heter " + name + ", du är " + age + " år och du " );
+            if (alive == true) {
+                Console.WriteLine("lever.");
+            }
+            else {
+                Console.WriteLine("lever inte.");
+            }
+            Console.ReadLine();
         }
     }
 }
