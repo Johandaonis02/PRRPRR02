52603b229247b3a9c3d5528e5be45ae05a16300f
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2020-09-01 12:32:59 +0200

    .

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -12,22 +12,40 @@ namespace HelloWorld
             {
                 var input = Console.ReadLine();
                 age = int.Parse(input);
-                if(age > 130){
-                    Console.WriteLine("no");
-                }
-                else{
+                if(age < 130)
+                {
                     break;
                 }
+                else
+                {
+                    Console.WriteLine("no");
+                }
             }
+            
             Console.WriteLine("Vad heter du?");
             var name = Console.ReadLine();
+
+            switch (name)
+            {
+                case "Johan":
+                    Console.WriteLine("Hallå där Bohan");
+                    break;
+                case "Björn":
+                    Console.WriteLine("Vet inte om ö funkar");
+                    break;
+                case "GW_Person":
+                    Console.WriteLine("Jag gillar dig!");
+                    break;
+                default:
+                    Console.WriteLine("Hallå där " + name);
+                    break;
+
+            }
+
             Console.WriteLine("Lever du? (No / Yes)");
             bool alive = false;
             if (Console.ReadLine().ToLower() == "yes") {
                 alive = true;
-            }
-            else if(Console.ReadLine().ToLower() != "no"){
-                Console.WriteLine("No? Ok")
             }
 
             Console.WriteLine("Du heter " + name + ", du är " + age + " år och du " );
@@ -39,12 +57,11 @@ namespace HelloWorld
             {
                 Console.WriteLine("lever inte.");
             }
-
             Console.WriteLine("Jag gillar jämna tal");
-            for (int i = 0; i < 10; i += 2){
+            for(int i = 0; i < 10; i += 2)
+            {
                 Console.WriteLine(i);
             }
-
             Console.ReadLine();
         }
     }
