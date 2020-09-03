e5f83adb358124d59aa933d9b387c57ba20a9ff7
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2020-09-03 10:26:06 +0200

    :)

--- HelloWorld2/HelloWorld2/Program.cs
+++ HelloWorld2/HelloWorld2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HelloWorld2
 {
@@ -6,7 +7,13 @@ namespace HelloWorld2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas");
+            string[] names = new string[5];
+
+            for (int i = 0; i < names.Length; i++){
+                names[i] = Console.ReadLine();
+            }  
+            
+            Console.WriteLine(names[0] + " är här, och " + names[1] + " är här, å så roligt att "+ names[2] +" är här. Och i vårt glada gänga har vi " + names[3] + " här igen, å så roligt att " + names[4] + " här...");
         }
     }
 }
