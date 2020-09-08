f27869562d8c95748f06ecc5190de60e1d1d4d22
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2020-09-08 12:42:14 +0200

    .

---
+++ Metoder/Metoder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Metoder
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+        }
+
+        private int Add(int num1, int num2){
+            return (num1 + num2);
+        }
+
+
+    }
+}
