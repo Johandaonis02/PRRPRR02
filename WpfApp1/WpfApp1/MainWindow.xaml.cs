506013fcf4197aaf09c38144e28759468b0a5e26
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2020-11-13 08:28:15 +0100

    Mini sak

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -33,54 +33,98 @@ namespace WpfApp1
             if (JaText.Text == "error"){
                 JaText.Text = "";
             }
-            
-            switch(Grid.GetColumn(button) + 4 * Grid.GetRow(button)){
+
+            switch (Grid.GetColumn(button) + 5 * Grid.GetRow(button)){
                 case 3:
-                    if (JaText.Text.Length != 0){
+                    if (JaText.Text.Length != 0)
+                    {
                         JaText.Text = JaText.Text.Remove(JaText.Text.Length - 1);
                     }
-                    else{
+                    else
+                    {
                         JaText.Text = "error";
                     }
                     break;
-                case 7:
+
+                case 8:
                     JaText.Text = "";
                     break;
-                case 11:
-                    if (JaText.Text[JaText.Text.Length - 1] != '+'){
-                        if (JaText.Text.Length != 0){
-                            JaText.Text += '+';
+
+                case 4:
+                case 9:
+                case 14:
+                case 19:
+                    char operatorMini = 'J';
+
+                    switch (Grid.GetColumn(button) + 5 * Grid.GetRow(button)){
+                        case 4:
+                            operatorMini = '+';
+                            break;
+                        case 9:
+                            operatorMini = '-';
+                            break;
+                        case 14:
+                            operatorMini = '*';
+                            break;
+                        case 19:
+                            operatorMini = '/';
+                            break;
+                    }
+
+                    if (JaText.Text[JaText.Text.Length - 1] != operatorMini)
+                    {
+                        if (JaText.Text.Length != 0)
+                        {
+                            JaText.Text += operatorMini;
                         }
                     }
                     break;
-                case 15:
+
+                case 18:
+                    JaText.Text += '+';
+                    char nextOperator = '+';
                     int result = 0;
                     int theNumberIWantToAdd = 0;
 
-                    while(JaText.Text.Length > 0) {
-                        if(JaText.Text[0] == '+'){
-                            result += theNumberIWantToAdd;
-                            theNumberIWantToAdd = 0;
-                        }
-                        else{
+                    while (JaText.Text.Length > 0)
+                    {
+                        if (JaText.Text[0] == '0' || JaText.Text[0] == '1' || JaText.Text[0] == '2' || JaText.Text[0] == '3' || JaText.Text[0] == '4' || JaText.Text[0] == '5' || JaText.Text[0] == '6' || JaText.Text[0] == '7' || JaText.Text[0] == '8' || JaText.Text[0] == '9')
+                        {
                             theNumberIWantToAdd = theNumberIWantToAdd * 10 + int.Parse(JaText.Text[0].ToString());
                         }
+                        else
+                        {
+                            switch (nextOperator)
+                            {
+                                case '+':
+                                    result += theNumberIWantToAdd;
+                                    break;
+                                case '-':
+                                    result -= theNumberIWantToAdd;
+                                    break;
+                                case '*':
+                                    result *= theNumberIWantToAdd;
+                                    break;
+                                case '/':
+                                    result /= theNumberIWantToAdd;
+                                    break;
+                            }
 
-                        JaText.Text.Remove(0);
+                            theNumberIWantToAdd = 0;
+                            nextOperator = JaText.Text[0];
+                        }
+
+                        JaText.Text = JaText.Text.Substring(1);
                     }
-                    
+                    //result = Math.Round(result * 100)/100;
+
                     JaText.Text = result.ToString();
-                   
+
                     break;
                 default:
                     JaText.Text += Grid.GetColumn(button) + 3 * Grid.GetRow(button);
                     break;
             }
-
-            
-        
-
         }
-
     }
 }
