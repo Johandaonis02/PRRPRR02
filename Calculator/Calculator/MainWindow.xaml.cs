f072ad612111139e780ffd5eee84ec2c79badb3c
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2020-11-29 21:33:35 +0100

    Fixed all the bug I have found.

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -30,23 +30,20 @@ namespace WpfApp1
             InitializeComponent();
         }
 
+        //This is a simple base 12 calculator
+
         private void OkButton_Click(object sender, RoutedEventArgs e)
         {
 
             var button = e.Source as Button;
-            if (DisplayOfNumbers.Text == "error"){
+            if (DisplayOfNumbers.Text == "Error"){
                 DisplayOfNumbers.Text = "";
             }
-
+            
             switch (Grid.GetColumn(button) + 5 * Grid.GetRow(button)){
                 case 3:
-                    if (DisplayOfNumbers.Text.Length != 0)
-                    {
+                    if (DisplayOfNumbers.Text.Length != 0){
                         DisplayOfNumbers.Text = DisplayOfNumbers.Text.Remove(DisplayOfNumbers.Text.Length - 1);
-                    }
-                    else
-                    {
-                        DisplayOfNumbers.Text = "error";
                     }
                     break;
 
@@ -55,33 +52,45 @@ namespace WpfApp1
                     break;
 
                 case 13:
-                    DisplayOfNumbers.Text += '.';
+                    if (DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '0' || DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '1' || DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '2' ||
+                        DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '3' || DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '4' || DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '5' || 
+                        DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '6' || DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '7' || DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '8' || 
+                        DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '9' || DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '↊' || DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] == '↋'){
+                        DisplayOfNumbers.Text += '.';
+                    }
                     break;
 
                 case 4:
                 case 9:
                 case 14:
                 case 19:
-                    char operatorMini = 'J';
 
-                    switch (Grid.GetColumn(button) + 5 * Grid.GetRow(button)){
-                        case 4:
-                            operatorMini = '+';
-                            break;
-                        case 9:
-                            operatorMini = '-';
-                            break;
-                        case 14:
-                            operatorMini = '*';
-                            break;
-                        case 19:
-                            operatorMini = '/';
-                            break;
-                    }
+                    if (DisplayOfNumbers.Text.Length != 0)
+                    {
+                        char operatorMini = 'J';
 
-                    if (DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] != '+' && DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] != '-' && DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] != '*' && DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] != '/'){
-                        if (DisplayOfNumbers.Text.Length != 0){
-                            DisplayOfNumbers.Text += operatorMini;
+                        switch (Grid.GetColumn(button) + 5 * Grid.GetRow(button))
+                        {
+                            case 4:
+                                operatorMini = '+';
+                                break;
+                            case 9:
+                                operatorMini = '-';
+                                break;
+                            case 14:
+                                operatorMini = '*';
+                                break;
+                            case 19:
+                                operatorMini = '/';
+                                break;
+                        }
+
+                        if (DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] != '+' && DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] != '-' && DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] != '*' && DisplayOfNumbers.Text[DisplayOfNumbers.Text.Length - 1] != '/')
+                        {
+                            if (DisplayOfNumbers.Text.Length != 0)
+                            {
+                                DisplayOfNumbers.Text += operatorMini;
+                            }
                         }
                     }
                     break;
@@ -96,16 +105,18 @@ namespace WpfApp1
                     while (DisplayOfNumbers.Text.Length > 0){
                         
                         if (duodecimals && !(DisplayOfNumbers.Text[0] == '+' || DisplayOfNumbers.Text[0] == '-' || DisplayOfNumbers.Text[0] == '*' || DisplayOfNumbers.Text[0] == '/')){
-
                             //Number after the decimal
-                            if (DisplayOfNumbers.Text[0] == '0' || DisplayOfNumbers.Text[0] == '1' || DisplayOfNumbers.Text[0] == '2' || DisplayOfNumbers.Text[0] == '3' || DisplayOfNumbers.Text[0] == '4' || DisplayOfNumbers.Text[0] == '5' || DisplayOfNumbers.Text[0] == '6' || DisplayOfNumbers.Text[0] == '7' || DisplayOfNumbers.Text[0] == '8' || DisplayOfNumbers.Text[0] == '9'){
-                                theNumberIWantToAdd += double.Parse(DisplayOfNumbers.Text[0].ToString()) * Math.Pow(1/12, positionBehindDuodecimal);
+                            if (DisplayOfNumbers.Text[0] == '0' || DisplayOfNumbers.Text[0] == '1' || DisplayOfNumbers.Text[0] == '2' ||
+                                DisplayOfNumbers.Text[0] == '3' || DisplayOfNumbers.Text[0] == '4' || DisplayOfNumbers.Text[0] == '5' ||
+                                DisplayOfNumbers.Text[0] == '6' || DisplayOfNumbers.Text[0] == '7' || DisplayOfNumbers.Text[0] == '8' ||
+                                DisplayOfNumbers.Text[0] == '9'){
+                                theNumberIWantToAdd += double.Parse(DisplayOfNumbers.Text[0].ToString()) * Math.Pow(1.0/12.0, positionBehindDuodecimal);
                             }
                             else if (DisplayOfNumbers.Text[0] == '↊'){
-                                theNumberIWantToAdd += 10 * Math.Pow(1/12, positionBehindDuodecimal);
+                                theNumberIWantToAdd += 10 * Math.Pow(1.0/12.0, positionBehindDuodecimal);
                             }
                             else if (DisplayOfNumbers.Text[0] == '↋'){
-                                theNumberIWantToAdd += 11 * Math.Pow(1/12, positionBehindDuodecimal);
+                                theNumberIWantToAdd += 11 * Math.Pow(1.0/12.0, positionBehindDuodecimal);
                             }
 
                             positionBehindDuodecimal++;
@@ -135,6 +146,9 @@ namespace WpfApp1
                                     result *= theNumberIWantToAdd;
                                     break;
                                 case '/':
+                                    if(theNumberIWantToAdd == 0){
+                                        DisplayOfNumbers.Text = "Error";
+                                    }
                                     result /= theNumberIWantToAdd;
                                     break;
                             }
@@ -192,19 +206,19 @@ namespace WpfApp1
                 dec = Math.Floor(dec/12) + dec - Math.Floor(dec);
             }
 
+            double MaxDuodecimals = 10;
+
             //Fix rounding issue
-            if (double.MaxValue > dec * Math.Pow(12, 10)){
-                dec *= Math.Pow(12, 10);
-                if (dec % 1 > 0.5){
-                    dec = Math.Ceiling(dec);
-                }
-                dec /= Math.Pow(12, 10);
+            if (double.MaxValue > dec * Math.Pow(12.0, MaxDuodecimals)){
+                dec *= Math.Pow(12, MaxDuodecimals);
+                dec = Math.Round(dec);
+                dec /= Math.Pow(12, MaxDuodecimals);
             }
 
             //Text after the duodecimal
             DuoDecResult += '.';
             double positionBehindDuodecimal = 0;
-            while (positionBehindDuodecimal < 10)
+            while (positionBehindDuodecimal < MaxDuodecimals)
             {
                 positionBehindDuodecimal++;
                 dec *= 12;
@@ -219,6 +233,15 @@ namespace WpfApp1
                     DuoDecResult = DuoDecResult + Math.Floor(dec);
                 }
                 dec -= Math.Floor(dec);
+            }
+
+            //Remove useless zeros
+            while(DuoDecResult.Last() == '0'){
+                DuoDecResult = DuoDecResult.Remove(DuoDecResult.Length - 1);
+            }
+            if (DuoDecResult.Last() == '.')
+            {
+                DuoDecResult = DuoDecResult.Remove(DuoDecResult.Length - 1);
             }
 
             return DuoDecResult;
