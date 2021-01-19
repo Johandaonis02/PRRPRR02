048df4fff98b2f6e5b7962ebf97fd8b11f6bc453
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2021-01-19 12:25:32 +0100

    .

---
+++ LaborationAbstraktion/LaborationAbstraktion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaborationAbstraktion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
