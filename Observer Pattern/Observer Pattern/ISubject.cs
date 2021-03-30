372f07e0fae0a5ccfb72aa2735719f12bba035b1
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2021-03-30 13:29:00 +0200

    Observer Pattern

---
+++ Observer Pattern/Observer Pattern/ISubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPatternDemo
+{
+    interface ISubject
+    {
+        void MailTo(Observer observer);
+        void StopMailTo(Observer observer);
+        void Notify();
+    }
+}
---
+++ Observer Pattern/Observer Pattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ObserverPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Subject();
+
+            Observer observer1 = new Observer("Karl");
+            subject.MailTo(observer1);
+
+            Observer observer2 = new Observer("Kalle");
+            subject.MailTo(observer2);
+
+            subject.Mail++;
+
+            Observer observer3 = new Observer("Knut");
+            subject.MailTo(observer3);
+
+            subject.Mail++;
+
+            subject.StopMailTo(observer3);
+
+            subject.Mail++;
+
+            Console.ReadLine();
+        }
+    }
+}
